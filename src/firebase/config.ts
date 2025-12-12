b11b1b43663675ee52cd4a42a3342209d1bc62b6
Haliyat Adeniyi <adeniyihaliyat716@gmail.com>
2025-12-12 07:14:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-983505805-400e5",
+  "appId": "1:618605675684:web:7f28c1d7817c810e6f5231",
+  "apiKey": "AIzaSyBnFbL1bAmQvvw_50UNC8XqwO1iR1FPITE",
+  "authDomain": "studio-983505805-400e5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "618605675684"
+};
